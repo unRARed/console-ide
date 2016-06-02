e75697da708d7c2c7220ff37428c24d3e68245b8
CSA Webmaster <webmaster@cloudsecurityalliance.org>
2016-06-02 13:51:16 -0700

    Add plugin to show trailing whitespace by default

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# Setup VIM
+
 mkdir ~/.vim
 mkdir ~/.vim/colors
 mv -i .vim/colors/* ~/.vim/colors/
 mv -i .vimrc ~/
-mv -i .tmux.conf ~/
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone git://github.com/slim-template/vim-slim.git ~/.vim/bundle/vim-slim
 git clone git@github.com:Yggdroot/indentLine.git ~/.vim/bundle/indent-line
+git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
 
+# Setup tmux 
+
+wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash
+mkdir ~/.bin
+mv tmuxinator.bash ~/.bin/
+mv -i .tmux.conf ~/
+gem install tmuxinator
+echo "source ~/.bin/tmuxinator.bash" >> ~/.bashrc
+echo "export EDITOR='vim'" >> ~/.bashrc
+echo "tmux attach || tmux new" >> ~/.bashrc
+source ~/.bashrc
