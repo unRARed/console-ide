a942ce7f6018870636d044ac6ffa5e232f9539d0
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-01-15 23:20:43 -0800

    Add ctrl->p plugin for fuzzy file search

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Setup VIM
+# Setup VIM Configuration files
 mkdir ~/.vim
 mkdir ~/.vim/colors
 cp -i .vim/colors/* ~/.vim/colors/
 cp -i .vimrc ~/
 
+# Install Pathogen for Plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-rm -Rf ~/.vim/bundle/vim-slim
-git clone git://github.com/slim-template/vim-slim.git ~/.vim/bundle/vim-slim
-
-rm -Rf ~/.vim/bundle/vim-elixir
-git clone git://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir
-
-rm -Rf ~/.vim/bundle/vim-slime-syntax
-git clone git@github.com:slime-lang/vim-slime-syntax.git ~/.vim/bundle/vim-slime-syntax
-
-rm -Rf ~/.vim/bundle/indent-line
-git clone git://github.com/Yggdroot/indentLine.git ~/.vim/bundle/indent-line
-
+# Base plugins
 rm -Rf ~/.vim/bundle/vim-better-whitespace
-git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
-
+git clone https://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
+rm -Rf ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+rm -Rf ~/.vim/bundle/indent-line
+git clone https://github.com/Yggdroot/indentLine.git ~/.vim/bundle/indent-line
 rm -Rf ~/.vim/bundle/emmet-vim
-git clone git://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+
+# Additional Syntax highlighting
+rm -Rf ~/.vim/bundle/vim-slim
+git clone https://github.com/slim-template/vim-slim.git ~/.vim/bundle/vim-slim
+rm -Rf ~/.vim/bundle/vim-elixir
+git clone https://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir
+rm -Rf ~/.vim/bundle/vim-slime-syntax
+git clone https://github.com/slime-lang/vim-slime-syntax.git ~/.vim/bundle/vim-slime-syntax
+
 
 # Setup tmux
 cp -i .tmux.conf ~/
