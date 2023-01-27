7ba1d98505660279c1f4a5201ce2b8cb789e72a8
Ryan Richardson <rrichardson@cloudsecurityalliance.org>
2023-01-26 16:53:13 -0800

    feat: reorganize config for neovim

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup VIM Configuration files
-rm -Rf ~/.vim
-mkdir -p ~/.vim/colors
-yes | cp -i .vimrc ~/
+rm -Rf ~/.config/nvim
+mkdir -p ~/.config/nvim/colors
+yes | cp -i .config/nvim/init.vim ~/.config/nvim/
 yes | cp -i .tmux.conf ~/
 
 if [ -x "$(command -v brew)" ]; then
@@ -13,87 +13,87 @@ if [ -x "$(command -v brew)" ]; then
 fi
 
 # Install Pathogen for Plugins
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle && \
+curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Color Schemes
-git clone https://github.com/sickill/vim-sunburst ~/.vim/sunburst
-mv ~/.vim/sunburst/colors/* ~/.vim/colors
-rm -Rf ~/.vim/sunburst
-git clone https://github.com/semibran/vim-colors-synthetic ~/.vim/synthetic
-mv ~/.vim/synthetic/colors/* ~/.vim/colors
-rm -Rf ~/.vim/synthetic
+git clone https://github.com/sickill/vim-sunburst ~/.config/nvim/sunburst
+mv ~/.config/nvim/sunburst/colors/* ~/.config/nvim/colors
+rm -Rf ~/.config/nvim/sunburst
+git clone https://github.com/semibran/vim-colors-synthetic ~/.config/nvim/synthetic
+mv ~/.config/nvim/synthetic/colors/* ~/.config/nvim/colors
+rm -Rf ~/.config/nvim/synthetic
 
 ######################
 ## Plugins - Global ##
 ######################
 
 # Block comments, see https://github.com/tpope/vim-commentary
-rm -Rf ~/.vim/bundle/vim-commentary
+rm -Rf ~/.config/nvim/bundle/vim-commentary
 git clone https://github.com/tpope/vim-commentary \
-  ~/.vim/bundle/vim-commentary
+  ~/.config/nvim/bundle/vim-commentary
 
 # Better start buffer
-rm -Rf ~/.vim/bundle/vim-startify
+rm -Rf ~/.config/nvim/bundle/vim-startify
 git clone https://github.com/mhinz/vim-startify \
-  ~/.vim/bundle/vim-startify
+  ~/.config/nvim/bundle/vim-startify
 
 # Better NetRW (Vinegar)
-rm -Rf ~/.vim/bundle/vinegar
+rm -Rf ~/.config/nvim/bundle/vinegar
 git clone https://github.com/tpope/vim-vinegar \
-  ~/.vim/bundle/vinegar
+  ~/.config/nvim/bundle/vinegar
 
 # Highlights (likely) unnecessary whitespace in buffer (and other tools)
-rm -Rf ~/.vim/bundle/vim-better-whitespace
+rm -Rf ~/.config/nvim/bundle/vim-better-whitespace
 git clone https://github.com/ntpeters/vim-better-whitespace \
-  ~/.vim/bundle/vim-better-whitespace
+  ~/.config/nvim/bundle/vim-better-whitespace
 
 # Fuzzy file finder with Ctrl->p
-rm -Rf ~/.vim/bundle/ctrlp.vim
+rm -Rf ~/.config/nvim/bundle/ctrlp.vim
 git clone https://github.com/ctrlpvim/ctrlp.vim \
-  ~/.vim/bundle/ctrlp.vim
-rm -Rf ~/.vim/bundle/ctrlp-py-matcher
+  ~/.config/nvim/bundle/ctrlp.vim
+rm -Rf ~/.config/nvim/bundle/ctrlp-py-matcher
 git clone https://github.com/FelikZ/ctrlp-py-matcher \
-  ~/.vim/bundle/ctrlp-py-matcher
+  ~/.config/nvim/bundle/ctrlp-py-matcher
 
 # Visual indication of inconsistent indentation
-rm -Rf ~/.vim/bundle/indent-line
+rm -Rf ~/.config/nvim/bundle/indent-line
 git clone https://github.com/Yggdroot/indentLine \
-  ~/.vim/bundle/indent-line
+  ~/.config/nvim/bundle/indent-line
 
 # Grep (find within files) replacement
-rm -Rf ~/.vim/bundle/vim-ripgrep
+rm -Rf ~/.config/nvim/bundle/vim-ripgrep
 # Temp fix for ngrep errors
 #      see: https://github.com/jremmen/vim-ripgrep/issues/57
 # git clone https://github.com/jremmen/vim-ripgrep \
 git clone --branch patch-1 https://github.com/lamchau/vim-ripgrep.git \
-  ~/.vim/bundle/vim-ripgrep
+  ~/.config/nvim/bundle/vim-ripgrep
 
 ##########################
 ## Plugins - Contextual ##
 ##########################
 
-rm -Rf ~/.vim/bundle/vim-rescript
+rm -Rf ~/.config/nvim/bundle/vim-rescript
 git clone https://github.com/rescript-lang/vim-rescript \
-  ~/.vim/bundle/vim-rescript
-rm -Rf ~/.vim/bundle/vim-rails
+  ~/.config/nvim/bundle/vim-rescript
+rm -Rf ~/.config/nvim/bundle/vim-rails
 git clone https://github.com/tpope/vim-rails \
-  ~/.vim/bundle/vim-rails
-rm -Rf ~/.vim/bundle/vim-coffee-script
+  ~/.config/nvim/bundle/vim-rails
+rm -Rf ~/.config/nvim/bundle/vim-coffee-script
 git clone https://github.com/kchmck/vim-coffee-script \
-  ~/.vim/bundle/vim-coffee-script
-rm -Rf ~/.vim/bundle/vim-better-sml
+  ~/.config/nvim/bundle/vim-coffee-script
+rm -Rf ~/.config/nvim/bundle/vim-better-sml
 git clone https://github.com/jez/vim-better-sml \
-  ~/.vim/bundle/vim-better-sml
-rm -Rf ~/.vim/bundle/vim-slim
+  ~/.config/nvim/bundle/vim-better-sml
+rm -Rf ~/.config/nvim/bundle/vim-slim
 git clone https://github.com/onemanstartup/vim-slim.git \
-  ~/.vim/bundle/vim-slim
-rm -Rf ~/.vim/bundle/vim-elixir
+  ~/.config/nvim/bundle/vim-slim
+rm -Rf ~/.config/nvim/bundle/vim-elixir
 git clone https://github.com/elixir-lang/vim-elixir.git \
-  ~/.vim/bundle/vim-elixir
-rm -Rf ~/.vim/bundle/vim-slime-syntax
+  ~/.config/nvim/bundle/vim-elixir
+rm -Rf ~/.config/nvim/bundle/vim-slime-syntax
 git clone https://github.com/slime-lang/vim-slime-syntax.git \
-  ~/.vim/bundle/vim-slime-syntax
-rm -Rf ~/.vim/bundle/dart-vim-plugin
+  ~/.config/nvim/bundle/vim-slime-syntax
+rm -Rf ~/.config/nvim/bundle/dart-vim-plugin
 git clone https://github.com/dart-lang/dart-vim-plugin.git \
-  ~/.vim/bundle/dart-vim-plugin
+  ~/.config/nvim/bundle/dart-vim-plugin
